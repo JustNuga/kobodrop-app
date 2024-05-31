b73e919c2706dfbc87ac6be60cadcb32e6fc5517
JustNuga <67005425+JustNuga@users.noreply.github.com>
2024-06-01 00:43:28 +0100

    Starting Point

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Nav from "./sections/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/Nav.tsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <>
+      <div className="text-blue-500  font-bold">Nav</div>
+    </>
+  );
+};
+
+export default Nav;
